d2b7dffcadfec82abdee2829900cb1cbee083e37
Ivan Rene Monroy Santos <ivan.monroy@sinco.com.co>
2025-11-10 17:33:13 -0500

    🟦separar implemetacion de recibo

---
+++ ReciboDeSupermercado/Recibo.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace ReciboDeSupermercado;
+
+public class Recibo
+{
+    private readonly Carrito _carrito;
+
+    public Recibo(Carrito carrito = null)
+    {
+        _carrito = carrito;
+    }
+
+    public string Generar()
+    {
+        if (_carrito == null)
+            return "TOTAL: 0.00€";
+
+        var recibo = new StringBuilder();
+        var productos = _carrito.ObtenerProductos();
+        foreach (var producto in productos)
+        {
+            var nombre = producto.Key.Nombre;
+            var cantidad = producto.Value;
+            var subtotal = producto.Key.Precio * cantidad;
+            recibo.AppendLine($"{nombre} x {cantidad} - {subtotal.ToString("F2", CultureInfo.InvariantCulture)}€");
+        }
+
+        recibo.AppendLine($"\nTOTAL: {_carrito.CalcularTotal().ToString("F2", CultureInfo.InvariantCulture)}€");
+        return recibo.ToString();
+    }
+}
--- ReciboDeSupermercado/ReciboDeSupermercadoTest.cs
+++ ReciboDeSupermercado/ReciboDeSupermercadoTest.cs
@@ -1,5 +1,4 @@
 ﻿using System.Globalization;
-using System.Text;
 using AwesomeAssertions;
 
 namespace ReciboDeSupermercado;
@@ -168,34 +167,5 @@ public class ReciboDeSupermercadoTest
         texto.Should().Contain("Frutos secos");
 
         texto.Should().Contain($"TOTAL: {_carrito.CalcularTotal().ToString("F2", CultureInfo.InvariantCulture)}€");
-    }
-}
-
-public class Recibo
-{
-    private readonly Carrito _carrito;
-
-    public Recibo(Carrito carrito = null)
-    {
-        _carrito = carrito;
-    }
-
-    public string Generar()
-    {
-        if (_carrito == null)
-            return "TOTAL: 0.00€";
-
-        var recibo = new StringBuilder();
-        var productos = _carrito.ObtenerProductos();
-        foreach (var producto in productos)
-        {
-            var nombre = producto.Key.Nombre;
-            var cantidad = producto.Value;
-            var subtotal = producto.Key.Precio * cantidad;
-            recibo.AppendLine($"{nombre} x {cantidad} - {subtotal.ToString("F2", CultureInfo.InvariantCulture)}€");
-        }
-
-        recibo.AppendLine($"\nTOTAL: {_carrito.CalcularTotal().ToString("F2", CultureInfo.InvariantCulture)}€");
-        return recibo.ToString();
     }
 }

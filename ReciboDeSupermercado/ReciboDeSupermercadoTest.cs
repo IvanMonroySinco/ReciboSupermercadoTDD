f6f6a1dbd941ced23b0e40606a72b7173d063f08
Ivan Rene Monroy Santos <ivan.monroy@sinco.com.co>
2025-11-10 10:10:30 -0500

    🟩Dado_CarritoVacioElTotal_Debe_SerCero

---
+++ ReciboDeSupermercado/ReciboDeSupermercadoTest.cs
@@ -0,0 +1,25 @@
+﻿using AwesomeAssertions;
+
+namespace ReciboDeSupermercado;
+
+public class ReciboDeSupermercadoTest
+{
+    [Fact]
+    public void Dado_CarritoVacioElTotal_Debe_SerCero()
+    {
+        Carrito carrito = new Carrito();
+
+        var valorTotal = carrito.CalcularTotal();
+        
+        valorTotal.Should().Be(0.0);
+
+    }
+}
+
+public class Carrito
+{
+    public double CalcularTotal()
+    {
+        return 0.0;
+    }
+}
